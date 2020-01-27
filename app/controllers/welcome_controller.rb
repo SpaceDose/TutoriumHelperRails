67af6cbe0854b8f8cda9155eb7caefc6f78a3592
Jan Ellermann <jan.ellermann@t-online.de>
2020-01-27 15:17:21 +0100

    Added modal.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,11 @@ class WelcomeController < ApplicationController
     @files = Dir.entries("./public/files/") - [".", "..", ".keep"]
   end
 
+  def new_file
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
 end
